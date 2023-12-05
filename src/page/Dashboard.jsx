e4d1677d9f8b2c1f875d1572a469e461d1e62095
phoebe <teng.phoebe0136@gmail.com>
2023-12-06 03:11:43 +0800

    added side bar and try to remove 1 index

--- src/page/Dashboard.jsx
+++ src/page/Dashboard.jsx
@@ -1,14 +1,22 @@
 import React, { useState } from "react"
 import Topbar from "../component/topbar.jsx"
+import Sidebarr from "../component/sidebar.jsx"
 import { Container, Box } from "@mui/material"
+
 const Dashboard = () => {
-   return (
-      <Container>
-         <Topbar />
-         <Box>
-            <h2>tehe</h2>
-         </Box>
-      </Container>
-   )
+  return (
+    <Container maxWidth="xl" disableGutters>
+      <Box display="flex" height="100vh">
+        {" "}
+        {/* Full viewport height */}
+        <Sidebarr />
+        <Box flexGrow={1}>
+          <Topbar />
+          {/* Other content goes here */}
+        </Box>
+      </Box>
+    </Container>
+  )
 }
+
 export default Dashboard
